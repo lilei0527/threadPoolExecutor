f7d1114af9b05f667bb9e0d1607a30691c2aa491
lilei <15623219507@163.com>
2020-05-27 14:38:04 +0800

    线程shutdown后唤醒阻塞在workerQueue.take()的线程，加入没有则将线程池的状态置为TIDYING

--- src/ThreadPoolExecutor.java
+++ src/ThreadPoolExecutor.java
@@ -11,7 +11,7 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author lilei
  **/
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused","SameParameterValue"})
 public class ThreadPoolExecutor {
     private volatile int maxThreadSize;
     private volatile int coreThreadSize;
@@ -20,13 +20,15 @@ public class ThreadPoolExecutor {
     private final ReentrantLock mainLock = new ReentrantLock();
     private AtomicInteger ctl = new AtomicInteger(ctlOf(RUNNING, 0));
 
-    private volatile boolean allowCoreThreadTimeOut = true;
+    private volatile boolean allowCoreThreadTimeOut = false;
 
     private static final int COUNT_BITS = Integer.SIZE - 3;
     private static final int CAPACITY = (1 << COUNT_BITS) - 1;
     private static final int RUNNING = -1 << COUNT_BITS;
     private static final int SHUTDOWN = 0;
     private static final int STOP = 1 << COUNT_BITS;
+    private static final int TIDYING    =  2 << COUNT_BITS;
+
 
     public ThreadPoolExecutor(int maxThreadSize, int coreThreadSize) {
         this.maxThreadSize = maxThreadSize;
@@ -49,9 +51,9 @@ public class ThreadPoolExecutor {
     }
 
 
-//    private static boolean runStateLessThan(int c, int s) {
-//        return c < s;
-//    }
+    private static boolean runStateLessThan(int c, int s) {
+        return c < s;
+    }
 
     private static boolean runStateAtLeast(int c, int s) {
         return c >= s;
@@ -106,7 +108,7 @@ public class ThreadPoolExecutor {
     private boolean addWorker(Runnable task, boolean core) {
         for (; ; ) {
             //如果目前的线程不是运行态,或者不是shutdown且队列不为空的情况添加worker失败
-            if (!isRunning(ctl.get()) && !((runStateOf(ctl.get()) == SHUTDOWN) && !workerQueue.isEmpty() && task != null)) {
+            if (!isRunning(ctl.get()) && !((runStateOf(ctl.get()) == SHUTDOWN) && !workerQueue.isEmpty() && task == null)) {
                 return false;
             }
             int c = ctl.get();
@@ -125,16 +127,56 @@ public class ThreadPoolExecutor {
             }
         }
 
-        Worker worker = new Worker(task);
-        workers.add(worker);
-        worker.thread.start();
-        return true;
+        boolean workerStarted = false;
+        boolean workerAdded = false;
+        Worker worker = null;
+        try {
+            worker = new Worker(task);
+            Thread t = worker.thread;
+            if (t != null) {
+                mainLock.lock();
+                try {
+                    int rs = runStateOf(ctl.get());
+                    if (rs < SHUTDOWN ||
+                            (rs == SHUTDOWN && task == null)) {
+                        if (t.isAlive())
+                            throw new IllegalThreadStateException();
+                        workers.add(worker);
+                        workerAdded = true;
+                    }
+                } finally {
+                    mainLock.unlock();
+                }
+
+                if (workerAdded) {
+                    t.start();
+                    workerStarted = true;
+                }
+            }
+        } finally {
+            if (!workerStarted)
+                addWorkerFailed(worker);
+        }
+        return workerStarted;
+    }
+
+
+    private void addWorkerFailed(Worker w) {
+        final ReentrantLock mainLock = this.mainLock;
+        mainLock.lock();
+        try {
+            if (w != null)
+                workers.remove(w);
+            decrementWorkerCount();
+        } finally {
+            mainLock.unlock();
+        }
     }
 
     Runnable getTask() {
+//        System.out.println("获取任务");
         boolean isTimeOut = false;
         for (; ; ) {
-            System.out.println("从等待队列查找任务。。。");
             //如果目前的线程池处于结束状态
             int c = ctl.get();
             int rs = runStateOf(c);
@@ -161,7 +203,6 @@ public class ThreadPoolExecutor {
                 }
                 isTimeOut = true;
             } catch (InterruptedException e) {
-                System.out.println("线程接受到终止命令");
                 isTimeOut = false;
             }
         }
@@ -171,12 +212,51 @@ public class ThreadPoolExecutor {
         this.allowCoreThreadTimeOut = allowCoreThreadTimeOut;
     }
 
+    final void tryTerminate() {
+        for (;;) {
+            int c = ctl.get();
+            if (isRunning(c) ||
+                    (runStateOf(c) == SHUTDOWN && ! workerQueue.isEmpty()))
+                return;
+            //在shutdown后可能有的线程阻塞在workerQueue.take(),需要线程阻塞的状态退出
+            if (workerCountOf(c) != 0) {
+                interruptIdleWorkers(true);
+                return;
+            }
+            if (ctl.compareAndSet(c, ctlOf(TIDYING, 0))) {
+                return;
+            }
+        }
+    }
+
+    private void interruptIdleWorkers(boolean onlyOne) {
+        final ReentrantLock mainLock = this.mainLock;
+        mainLock.lock();
+        try {
+            for (Worker w : workers) {
+                Thread t = w.thread;
+                if (!t.isInterrupted() && w.tryLock()) {
+                    try {
+                        t.interrupt();
+                    } catch (SecurityException ignore) {
+                    } finally {
+                        w.unlock();
+                    }
+                }
+                if (onlyOne)
+                    break;
+            }
+        } finally {
+            mainLock.unlock();
+        }
+    }
 
     private class Worker extends AbstractQueuedSynchronizer implements Runnable {
         private Runnable task;
         private Thread thread;
 
         public Worker(Runnable task) {
+            setState(-1);
             this.task = task;
             this.thread = new Thread(this);
         }
@@ -214,12 +294,15 @@ public class ThreadPoolExecutor {
         @Override
         public void run() {
             Thread wt = Thread.currentThread();
+            Runnable task = this.task;
+            this.task = null;
+            this.unlock();
             while (task != null || (task = getTask()) != null) {
                 //上锁是为了shutdown命令不会中断正在运行的线程
                 this.lock();
-                if ((runStateAtLeast(ctl.get(), STOP) ||
+                if ((runStateAtLeast(ctl.get(), SHUTDOWN) ||
                         (Thread.interrupted() &&
-                                runStateAtLeast(ctl.get(), STOP))) &&
+                                runStateAtLeast(ctl.get(), SHUTDOWN))) &&
                         !wt.isInterrupted())
                     wt.interrupt();
                 try {
@@ -235,9 +318,17 @@ public class ThreadPoolExecutor {
             //任务队列为空
             workers.remove(this);
 
-            int w = workerCountOf(ctl.get());
-            if ((!allowCoreThreadTimeOut && w < coreThreadSize) || w == 0) {
-                addWorker(null, true);
+
+            tryTerminate();
+            int c = ctl.get();
+
+            if (runStateLessThan(c, STOP)) {
+                int min = allowCoreThreadTimeOut ? 0 : coreThreadSize;
+                if (min == 0 && !workerQueue.isEmpty())
+                    min = 1;
+                if (workerCountOf(c) >= min)
+                    return; // replacement not needed
+                addWorker(null, false);
             }
         }
     }
@@ -261,9 +352,9 @@ public class ThreadPoolExecutor {
         } finally {
             mainLock.unlock();
         }
+        tryTerminate();
     }
 
-    @SuppressWarnings("SameParameterValue")
     private void advanceRunState(int targetState) {
         for (; ; ) {
             int c = ctl.get();
@@ -278,13 +369,15 @@ public class ThreadPoolExecutor {
     public static void main(String[] args) {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 3);
 
+//        java.util.concurrent.ThreadPoolExecutor threadPoolExecutor = new java.util.concurrent.ThreadPoolExecutor
+//                (3,3,1000,TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>());
+//        threadPoolExecutor.allowCoreThreadTimeOut(true);
         threadPoolExecutor.execute(() -> {
             for (int i = 0; i < 10; i++) {
                 System.out.println(i);
                 try {
                     Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
                 }
             }
         });
@@ -295,8 +388,7 @@ public class ThreadPoolExecutor {
                 System.out.println(i);
                 try {
                     Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
                 }
             }
         });
@@ -307,8 +399,7 @@ public class ThreadPoolExecutor {
                 System.out.println(i);
                 try {
                     Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
                 }
             }
         });
@@ -318,33 +409,17 @@ public class ThreadPoolExecutor {
                 System.out.println(i);
                 try {
                     Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
                 }
             }
         });
+
+
+
 
         threadPoolExecutor.shutdown();
 
-        threadPoolExecutor.execute(() -> {
-            for (int i = 40; i < 50; i++) {
-                System.out.println(i);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
 
-        while (true) {
-            System.out.println("目前工作线程的数量：" + workerCountOf(threadPoolExecutor.ctl.get()));
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
 
     }
 }
